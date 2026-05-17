40d99023afb804ebb6ba1b1119d8e10a73cfb80d
CesarPan720 <angheloperichetimoteo@gmail.com>
2026-05-17 13:15:48 -0500

    Solo borre llaves del pool

--- backend/src/models/Articulo.js
+++ backend/src/models/Articulo.js
@@ -1,4 +1,4 @@
-const { pool } = require('../config/database');
+const pool = require('../config/database');
 
 const crearArticulo = async (datosArticulo) => {
     const connection = await pool.getConnection();
--- backend/src/models/Inventario.js
+++ backend/src/models/Inventario.js
@@ -1,4 +1,4 @@
-const { pool } = require('../config/database');
+const pool  = require('../config/database');
 
 const obtenerInventarioCompleto = async () => {
     const query = `
