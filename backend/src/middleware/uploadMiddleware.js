c8793069b79b64ba7c81ff66c8f5e228673bc9ba
Bryamtf <carlosterrones05@gmail.com>
2026-05-31 18:37:34 -0500

    feat: agregr soporte paracarga de imagenes en formato WEBP y mejorar la logica de filtrado de archivos

--- backend/src/middleware/uploadMiddleware.js
+++ backend/src/middleware/uploadMiddleware.js
@@ -1,25 +1,23 @@
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
 
-// Función auxiliar para crear carpetas si no existen
 const asegurarCarpeta = (ruta) => {
-    if (!fs.existsSync(ruta)) {
-        fs.mkdirSync(ruta, { recursive: true });
-    }
+  if (!fs.existsSync(ruta)) {
+    fs.mkdirSync(ruta, { recursive: true });
+  }
 };
 
-// 1. Configuración ESPECÍFICA para Artículos
 const storageArticulos = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const ruta = path.join(__dirname, '../../uploads/articulos/temp');
-        asegurarCarpeta(ruta);
-        cb(null, ruta);
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, 'temp-' + uniqueSuffix + path.extname(file.originalname));
-    }
+  destination: function (req, file, cb) {
+    const ruta = path.join(__dirname, "../../uploads/articulos/temp");
+    asegurarCarpeta(ruta);
+    cb(null, ruta);
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, "temp-" + uniqueSuffix + path.extname(file.originalname));
+  },
 });
 
 const fileFilter = (req, file, cb) => {
@@ -53,9 +51,26 @@ const storageCotizaciones = multer.diskStorage({
   },
 });
 
-const uploadArticulos = multer({ 
-  storage: storageArticulos, 
-  fileFilter: fileFilter 
+const storageImagenesCotizaciones = multer.memoryStorage();
+
+const fileFilterImagenes = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png|webp/;
+  const extname = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase(),
+  );
+  const mimetype = allowedTypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb(new Error("Solo se permiten imágenes JPG, PNG o WEBP"), false);
+  }
+};
+
+// Middlewares
+const uploadArticulos = multer({
+  storage: storageArticulos,
+  fileFilter: fileFilter,
 });
 
 const uploadCotizaciones = multer({
@@ -64,7 +79,14 @@ const uploadCotizaciones = multer({
   fileFilter: fileFilter,
 });
 
-module.exports = {
-    uploadArticulos, uploadCotizaciones
-};
+const uploadImagenesCotizacion = multer({
+  storage: storageImagenesCotizaciones,
+  limits: { fileSize: 5 * 1024 * 1024 },
+  fileFilter: fileFilterImagenes,
+});
 
+module.exports = {
+  uploadArticulos,
+  uploadCotizaciones,
+  uploadImagenesCotizacion,
+};
