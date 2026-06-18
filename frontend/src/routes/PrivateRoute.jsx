643c90b63d1b2a534d090ee883752df3a929c0f9
CesarPan720 <angheloperichetimoteo@gmail.com>
2026-06-18 11:55:08 -0500

    Error intencional 2

--- frontend/src/routes/PrivateRoute.jsx
+++ frontend/src/routes/PrivateRoute.jsx
@@ -3,5 +3,5 @@ import { useAuth } from '@/features/auth/contexts/AuthContext';
 
 export default function PrivateRoute() {
   const { isAuthenticated } = useAuth();
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+  return isAuthenticated ? <Outlet  : <Navigate to="/login" replace />;
 }
