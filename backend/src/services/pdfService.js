d67919ee47d45d22aca3ef49edd28b3574e7ebf9
Bryamtf <carlosterrones05@gmail.com>
2026-06-20 15:57:34 -0500

    fix: se recibe datos de la empresa de forma dinamica

--- backend/src/services/pdfService.js
+++ backend/src/services/pdfService.js
@@ -1,18 +1,11 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 const path = require("path");
-
+const empresa = require("../config/empresa");
+const formatFecha = require("../utils/formatFecha");
 class PDFService {
   constructor() {
-    this.empresa = {
-      nombre: "GRUPO AUTONORT PERU SAC",
-      ruc: "20612370932",
-      direccion:
-        "AV. PROLONGACION CIEZA DE LEON MZA. 30 LOTE: 10 URB. LA PURISIMA - CHICLAYO- LAMBAYEQUE",
-      telefono: "(074) 234-5678",
-      email: "info@autonort.com",
-      website: "www.autonort.com",
-    };
+    this.empresa = empresa;
     this.browser = null;
     this.logoHTML = this.cargarLogoHTML();
   }
@@ -398,7 +391,7 @@ class PDFService {
         </tr>
         <tr>
           <td>FECHA:</td>
-          <td>${this.escaparHTML(cotizacion.fecha_emision)}</td>
+          <td>${formatFecha(cotizacion.fecha_emision)}</td>
         </tr>
         <tr>
           <td>MONEDA:</td>
